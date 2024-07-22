d43cf96cb054def8aa8c6057ad31fa2d16432bc5
Leyang Xue <leyang.xue@ed.ac.uk>
2024-07-22 14:12:00 +0100

    add new interface for vllm

--- serverless_llm/store/serverless_llm_store/sllm_store.py
+++ serverless_llm/store/serverless_llm_store/sllm_store.py
@@ -21,7 +21,7 @@ import json
 import os
 import time
 import uuid
-from typing import Optional, Union
+from typing import Optional, Union, Dict
 
 import torch
 from accelerate import dispatch_model, init_empty_weights
@@ -60,6 +60,32 @@ logger = init_logger(__name__)
 
 def _get_uuid():
     return str(uuid.uuid4())
+
+
+def save_dict(model_state_dict: Dict[str, torch.Tensor], model_path: str):
+    tensor_names = list(model_state_dict.keys())
+    tensor_data_index = {}
+    for name, param in model_state_dict.items():
+        param_storage = param.untyped_storage()
+        data_ptr = param_storage.data_ptr()
+        size = param_storage.size()
+        tensor_data_index[name] = (data_ptr, size)
+        
+    if not os.path.exists(model_path):
+        os.makedirs(model_path, exist_ok=True)
+
+    # save tensors
+    tensor_offsets = save_tensors(tensor_names, tensor_data_index, model_path)
+
+    # create tensor index
+    tensor_index = {}
+    for name, param in model_state_dict.items():
+        # name: offset, size
+        tensor_index[name] = (tensor_offsets[name], tensor_data_index[name][1], tuple(param.shape), tuple(param.stride()), str(param.dtype))
+
+    # save tensor index
+    with open(os.path.join(model_path, "tensor_index.json"), "w") as f:
+        json.dump(tensor_index, f)
 
 
 def save_model(model: nn.Module, model_path: str):
@@ -343,6 +369,85 @@ def load_dict(
 
     return state_dict
 
+
+def request_load_into_gpu(
+    model_name_or_path: Optional[Union[str, os.PathLike]],
+    device_map: DeviceMapType = "auto",
+    storage_path: str = "./models",
+):
+    client = SllmStoreClient("localhost:8073")
+    
+    with open(
+        os.path.join(storage_path, model_name_or_path, "tensor_index.json"), "r"
+    ) as f:
+        tensor_index = json.load(f)
+
+    tensor_meta_index = {}
+    tensor_data_index = {}
+    for name, (offset, size, shape, stride, dtype) in tensor_index.items():
+        tensor_meta_index[name] = (shape, stride, dtype)
+        tensor_data_index[name] = (offset, size)
+
+    start = time.time()
+    expanded_device_map = _expand_tensor_name(
+        device_map, list(tensor_index.keys())
+    )
+    device_memory = calculate_device_memory(
+        expanded_device_map, tensor_data_index
+    )
+    # logger.debug(f"calculate_device_memory {device_memory}")
+    cuda_memory_ptrs = allocate_cuda_memory(device_memory)
+    # cuda_memory_ptrs = { k: [v] for k,v in cuda_memory_ptrs.items()}
+    cuda_memory_handles = get_cuda_memory_handles(cuda_memory_ptrs)
+    device_uuid_map = get_device_uuid_map()
+    # logger.debug(f"determine device_uuid_map {device_uuid_map}")
+    tensor_device_offsets, tensor_copy_chunks = calculate_tensor_device_offsets(
+        expanded_device_map, tensor_data_index
+    )
+    logger.debug(f"allocate_cuda_memory takes {time.time() - start} seconds")
+
+    replica_uuid = _get_uuid()
+    ret = client.load_into_gpu(
+        model_name_or_path,
+        replica_uuid,
+        {
+            device_uuid_map[device_id]: v
+            for device_id, v in tensor_copy_chunks.items()
+        },
+        {
+            device_uuid_map[device_id]: [v]
+            for device_id, v in cuda_memory_handles.items()
+        },
+    )
+    if not ret or ret == False:
+        raise ValueError(f"Failed to load model {model_name_or_path} into GPU")
+    
+    start = time.time()
+    state_dict = restore_tensors(
+        tensor_meta_index, cuda_memory_ptrs, tensor_device_offsets
+    )
+    logger.info(f"restore state_dict takes {time.time() - start} seconds")
+    
+    return replica_uuid, state_dict, device_map
+
+def load_dict_single_device(
+    model_name_or_path: Optional[Union[str, os.PathLike]],
+    storage_path: str = "./models",
+):
+    # all tensor load to one device
+    device_id = torch.cuda.current_device()
+    device_map = {"": device_id}
+    
+    client = SllmStoreClient("localhost:8073")
+    ret = client.load_into_cpu(model_name_or_path)
+    if not ret or ret == False:
+        raise ValueError(f"Failed to load model {model_name_or_path} into CPU")
+    
+    replica_uuid, state_dict, device_map = request_load_into_gpu(model_name_or_path, device_map, storage_path)
+
+    client.confirm_model_loaded(model_name_or_path, replica_uuid)
+
+    return state_dict
 
 def load_dict_non_blocking(
     model_name_or_path: Optional[Union[str, os.PathLike]],
