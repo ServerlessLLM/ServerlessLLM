adb0abad93492797efcc958dbe9cf4c54ee62042
future-xy <fuyao3860@gmail.com>
2024-08-14 01:34:56 +0100

    fix: add sudo

--- .github/workflows/scripts/cuda-install.sh
+++ .github/workflows/scripts/cuda-install.sh
@@ -7,11 +7,11 @@ OS=$(echo $2 | tr -d ".\-")
 
 # Installs CUDA
 wget -nv https://developer.download.nvidia.com/compute/cuda/repos/${OS}/x86_64/cuda-keyring_1.1-1_all.deb
-dpkg -i cuda-keyring_1.1-1_all.deb
+sudo dpkg -i cuda-keyring_1.1-1_all.deb
 rm cuda-keyring_1.1-1_all.deb
-apt -qq update
-apt -y install cuda-${cuda_version} cuda-nvcc-${cuda_version} cuda-libraries-dev-${cuda_version}
-apt clean
+sudo apt -qq update
+sudo apt -y install cuda-${cuda_version} cuda-nvcc-${cuda_version} cuda-libraries-dev-${cuda_version}
+sudo apt clean
 
 # Test nvcc
 PATH=/usr/local/cuda-$1/bin:${PATH}
