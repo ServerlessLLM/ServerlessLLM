c0dc04acbbe9f2eafd6c193e654c13f4c419c153
future-xy <fuyao3860@gmail.com>
2024-08-30 03:31:34 +0100

    docs: remove todo

--- serverless_llm/store/serverless_llm_store/sllm_store.py
+++ serverless_llm/store/serverless_llm_store/sllm_store.py
@@ -15,7 +15,6 @@
 #  See the License for the specific language governing permissions and         #
 #  limitations under the License.                                              #
 # ---------------------------------------------------------------------------- #
-# TODO: Add license
 import concurrent.futures
 import json
 import os
