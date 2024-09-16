44497ea1745436bcbfb478e1d5ba0f9e2c216b2f
future-xy <fuyao3860@gmail.com>
2024-09-16 16:56:00 +0100

    fix: ignore applied patch

--- serverless_llm/store/patch.sh
+++ serverless_llm/store/patch.sh
@@ -26,4 +26,4 @@ if [ ! -f "$PATCH_FILE" ]; then
 fi
 
 VLLM_PATH=$(python -c "import vllm; import os; print(os.path.dirname(os.path.abspath(vllm.__file__)))")
-patch -p2 -d $VLLM_PATH < $PATCH_FILE
+patch -N -p2 -d $VLLM_PATH < $PATCH_FILE
