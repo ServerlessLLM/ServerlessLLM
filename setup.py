25bd42e91feef8e03f5b1dfe696bb5960f82459c
future-xy <yao.fu.aisys@gmail.com>
2024-10-21 16:35:05 +0100

    chore: bump version number to 0.5.0

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ sys.path.append(Path.cwd().as_posix())
 
 setup(
     name="serverless-llm",
-    version="0.4.1",
+    version="0.5.0",
     install_requires=install_requires,
     long_description=read_readme(),
     long_description_content_type="text/markdown",
