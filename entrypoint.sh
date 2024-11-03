ab809fbdbe33d5e81221dfd68b21bccf45e393e8
Yao Fu <yao.fu.aisys@gmail.com>
2024-11-03 16:25:09 +0000

    docs: add Docker Compose example
    - Add `docker-compose.yml` file for easier setup
    - Add README instructions for using Docker Compose

--- entrypoint.sh
+++ entrypoint.sh
@@ -35,7 +35,7 @@ initialize_head_node() {
   RAY_NUM_CPUS="${RAY_NUM_CPUS:-$DEFAULT_RAY_NUM_CPUS}"
 
   # Construct the command
-  CMD="ray start --head --port=$RAY_PORT --resources='$RAY_RESOURCES' --num-cpus=$RAY_NUM_CPUS --block"
+  CMD="ray start --head --port=$RAY_PORT --resources='$RAY_RESOURCES' --num-cpus=$RAY_NUM_CPUS"
 
   # Display and execute the command
   echo "Executing: $CMD"
@@ -48,11 +48,10 @@ initialize_worker_node() {
 
   # Patch the vLLM code
   VLLM_PATH=$(python -c "import vllm; import os; print(os.path.dirname(os.path.abspath(vllm.__file__)))")
-  patch -p2 -d $VLLM_PATH < ./sllm_store/vllm_patch/sllm_load.patch
+  patch -p2 -d $VLLM_PATH < ./vllm_patch/sllm_load.patch
 
   # Start checkpoint store
   STORAGE_PATH="${STORAGE_PATH:-$DEFAULT_STORAGE_PATH}"
-  # TODO: Temporary remove the registration required flag, as registration is not working for vLLM backend
   CMD="sllm-store-server -storage_path=$STORAGE_PATH -registration_required=true &"
   # CMD="sllm-store-server -storage_path=$STORAGE_PATH &"
   echo "Executing: $CMD"
@@ -69,7 +68,7 @@ initialize_worker_node() {
   RAY_RESOURCES='{"worker_node": 1, "worker_id_'$WORKER_ID'": 1}'
 
   # Construct the command
-  CMD="ray start --address=$RAY_HEAD_ADDRESS --resources='$RAY_RESOURCES' --block"
+  CMD="ray start --address=$RAY_HEAD_ADDRESS --resources='$RAY_RESOURCES'"
 
   # Display and execute the command
   echo "Executing: $CMD"
