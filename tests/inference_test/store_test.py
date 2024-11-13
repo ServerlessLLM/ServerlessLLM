05be7eedcdb2c0ad0d09fa19efe469bda63389f4
tsuiusi <ryantsui786@gmail.com>
2024-11-13 23:53:55 +0000

    fix: missing import

--- tests/inference_test/store_test.py
+++ tests/inference_test/store_test.py
@@ -2,7 +2,7 @@ import os
 import sys
 import json
 import subprocess
-from typing import Optional, Dict, Any
+from typing import Optional, Dict, Any, List
 
 from sllm_store.transformers import save_model
 
