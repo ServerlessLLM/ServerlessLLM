bb4950104ba6fbee158fe886a18a0e020a4625ad
dalongbao <ryantsui786@gmail.com>
2024-12-29 00:55:22 +0000

    test: saving tests

--- test/qtest3.py
+++ test/qtest3.py
@@ -16,14 +16,8 @@ for i in range(num_gpus):
     torch.cuda.synchronize()
 
 before_mem = torch.cuda.memory_allocated()
-model = load_model(
-    model_name,
-    device_map="auto",
-    storage_path=model_folder,
-    fully_parallel=True,
-)
 print(f"getting model from {model_folder}")
-# model = load_model(model_name, device_map="auto", storage_path=model_folder, fully_parallel=True, quantization="int4")
+model = load_model(model_name, device_map="auto", storage_path=model_folder, fully_parallel=True, quantization="int8")
 after_mem = torch.cuda.memory_allocated()
 print(f"Memory difference: {after_mem - before_mem}")
 print(f"memory footprint: {model.get_memory_footprint()}")
