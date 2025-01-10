fe9a743ffc71496cda5e1d30e55cbf4554c98068
XuehengWang <xwang2395@wisc.edu>
2025-01-10 00:08:00 +0000

    update router and scheduler

--- sllm/serve/schedulers/fcfs_scheduler.py
+++ sllm/serve/schedulers/fcfs_scheduler.py
@@ -144,17 +144,27 @@ class FcfsScheduler(SllmScheduler):
                     allocation_result,
                 ) in loading_requests:
                     allocated = False
+                    keep_going = False
                     for node_id, node_info in worker_nodes.items():
-                        if node_info["free_gpu"] >= num_gpus:
-                            async with self.queue_lock:
-                                self.model_loading_queues[model_name].pop(idx)
+                        async with self.queue_lock:
+                            if node_info["free_gpu"] >= num_gpus:
+                                if allocation_result.done():
+                                    keep_going = True
+                                    break
+                                try:
+                                    self.model_loading_queues[model_name].remove((request_time, num_gpus, allocation_result))
+                                except ValueError: 
+                                    keep_going = True
+                                    break                       
                                 allocation_result.set_result(node_id)
-                            allocated = True
-                            logger.info(
-                                f"Allocated node {node_id} for model {model_name}"
-                            )
-                            node_info["free_gpu"] -= num_gpus
+                                allocated = True
+                                logger.info(
+                                    f"Allocated node {node_id} for model {model_name}"
+                                )
+                                node_info["free_gpu"] -= num_gpus
                             break
+                    if keep_going:
+                        continue
                     if not allocated:
                         logger.info(f"No available node for model {model_name}")
                 await self._update_worker_nodes(worker_nodes)
