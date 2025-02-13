65b141abb391afa597f01799d4d0569d177136be
Siyang Shao <siyang.shao@outlook.com>
2025-02-13 12:35:29 +0800

    fix: add entrypoint

---
+++ sllm_store/examples/save_transformers_model.py
@@ -0,0 +1,37 @@
+import argparse
+import os
+
+import torch
+from transformers import AutoModelForCausalLM
+
+from sllm_store.transformers import save_model
+
+parser = argparse.ArgumentParser(
+    description="Save a model from HuggingFace model hub."
+)
+parser.add_argument(
+    "--model_name",
+    type=str,
+    required=True,
+    help="Model name from HuggingFace model hub.",
+)
+parser.add_argument(
+    "--storage_path",
+    type=str,
+    default="/models",
+    help="Local path to save the model.",
+)
+
+args = parser.parse_args()
+
+model_name = args.model_name
+storage_path = args.storage_path
+
+# Load a model from HuggingFace model hub.
+model = AutoModelForCausalLM.from_pretrained(
+    model_name, torch_dtype=torch.float16
+)
+
+# Save the model to the local path.
+model_path = os.path.join(storage_path, model_name)
+save_model(model, model_path)
