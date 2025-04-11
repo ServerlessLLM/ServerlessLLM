f47af58f79fdaa0eb91356926f8c562fa355892c
MartinRepo <1191731324@qq.com>
2025-04-11 19:32:22 +0100

    fix typo in transformers_bachend.py

--- sllm/serve/backends/transformers_backend.py
+++ sllm/serve/backends/transformers_backend.py
@@ -284,7 +284,6 @@ class TransformersBackend(SllmBackend):
         messages = request_data.get("messages", [])
         temperature = request_data.get("temperature", 0.7)
         max_tokens = request_data.get("max_tokens", 10)
-        lora_adapter = request_data.get("lora_adapter", "")
 
         # Combine messages to form the prompt
         try:
