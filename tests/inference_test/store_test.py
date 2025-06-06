2ce0c778e2b5ea343a25d480fe6130ce2d5a1933
dalongbao <ryantsui786@gmail.com>
2025-06-06 07:38:11 +0100

    fix: wrapped path with str()

--- tests/inference_test/store_test.py
+++ tests/inference_test/store_test.py
@@ -30,7 +30,7 @@ def store_and_compare(model_name, storage_path):
         hf_model = AutoModelForCausalLM.from_pretrained(
             model_name, torch_dtype=torch.float16, trust_remote_code=True
         )
-        save_model(hf_model, cache_dir)
+        save_model(hf_model, str(cache_dir))
 
         test_model = load_model(
             model_name,
