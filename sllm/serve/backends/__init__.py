80ccc882087f5d5ff3ed41f32972351875cd4e5b
FionaY728 <utachan899@gmail.com>
2025-07-09 20:58:29 +0100

    Update backends __init__.py
    - Add necessary imports or exports for backend modules
    - Ensure proper module initialization
    - Support for SGLang and Transformers backends

--- sllm/serve/backends/__init__.py
+++ sllm/serve/backends/__init__.py
@@ -1,26 +1,3 @@
-# # ---------------------------------------------------------------------------- #
-# #  serverlessllm                                                               #
-# #  copyright (c) serverlessllm team 2024                                       #
-# #                                                                              #
-# #  licensed under the apache license, version 2.0 (the "license");             #
-# #  you may not use this file except in compliance with the license.            #
-# #                                                                              #
-# #  you may obtain a copy of the license at                                     #
-# #                                                                              #
-# #                  http://www.apache.org/licenses/license-2.0                  #
-# #                                                                              #
-# #  unless required by applicable law or agreed to in writing, software         #
-# #  distributed under the license is distributed on an "as is" basis,           #
-# #  without warranties or conditions of any kind, either express or implied.    #
-# #  see the license for the specific language governing permissions and         #
-# #  limitations under the license.                                              #
-# # ---------------------------------------------------------------------------- #
-# from .dummy_backend import DummyBackend
-# from .transformers_backend import TransformersBackend
-# from .vllm_backend import VllmBackend
-# from .sglang_backend import SGLangBackend, SGLangMode
-# __all__ = ["DummyBackend", "VllmBackend", "TransformersBackend"," SGLangBackend", "SGLangMode"]
-
 # ---------------------------------------------------------------------------- #
 #  serverlessllm                                                               #
 #  copyright (c) serverlessllm team 2024                                       #
@@ -51,4 +28,10 @@ backend_registry = {
     "vllm": VllmBackend,
 }
 
-__all__ = ["DummyBackend", "SGLangBackend", "VllmBackend", "TransformersBackend", "backend_registry"]
+__all__ = [
+    "DummyBackend",
+    "SGLangBackend",
+    "VllmBackend",
+    "TransformersBackend",
+    "backend_registry",
+]
