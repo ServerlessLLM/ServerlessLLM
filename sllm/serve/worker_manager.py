159d7709877123c80bdebed5ce24477726605298
tsuiusi <ryantsui786@gmail.com>
2025-07-24 04:41:12 +0100

    fix: capitalized P accidentally

--- sllm/serve/worker_manager.py
+++ sllm/serve/worker_manager.py
@@ -15,7 +15,7 @@
 #  limitations under the License.                                              #
 # ---------------------------------------------------------------------------- #
 
-import aiohttP
+import aiohttp
 import asyncio
 import json
 import random
